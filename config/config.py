0d14d54bc63dc1655d2561e559eb4a70f0db662c
Abir Hasan <hackitabirhasan@gmail.com>
2021-02-10 04:11:58 +0600

    Update and rename sample_config.py to config.py

--- config/config.py
+++ config/config.py
@@ -2,24 +2,23 @@
 from pyrogram import filters
 
 # Get these two from https://my.telegram.org
-API_ID = 1234567
-API_HASH = "ab1c23def45fg67890h123i45678j9kl"
+API_ID = 1578262
+API_HASH = "664ecb8d62405ae3e3e015216f6e2615"
 
 # Get this from @Botfather
-TOKEN = "1234567890:ABCdEFgHij1KlMNop_QrStuVWxyzuA-EmXI"
+TOKEN = "1325820510:AAFlkHgB_xioveMAqZ-E_U88vsB0_35xH40"
 
 # Your MongoDB URI (using a database named "vcpb"), if you don't provide, you can't use the playlist feature and wont see now playing message
-MONGO_DB_URI = ""
+MONGO_DB_URI = "mongodb+srv://userbot:userbot@cluster0.hwmx5.mongodb.net/cluster0?retryWrites=true&w=majority"
 
 # The IDs of the users which can stream, skip, pause and change volume
 SUDO_USERS = [
-    383407735,
-    951435494,
-    1392620345
+    1445283714,
+    715779594
 ]
 
 # The ID of the group where your bot streams
-GROUP = -1001402753006
+GROUP = -1001256038785
 
 # Users must join the group before using the bot (note: the bot should be admin in the group if you enable this)
 USERS_MUST_JOIN = False
@@ -28,7 +27,7 @@ USERS_MUST_JOIN = False
 LANG = "en"
 
 # Max video duration allowed for user downloads in minutes
-DUR_LIMIT = 5
+DUR_LIMIT = 15
 
 # No need to touch the following.
 LOG_GROUP = GROUP if MONGO_DB_URI != "" else None
